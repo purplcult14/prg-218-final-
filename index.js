8cf66c60ddcddd973865fd88924d2615dff856b1
purplcult14 <emmabyblow@gmail.com>
2024-12-12 22:16:56 -0700

    mewo

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
-home.addEventListener('click', function(){
-    console.log('test')
+const button = document.getElementById('submit')
+button.addEventListener("click", submitting)
+function submitting (){
+    alert("Thank you for your message! :)")
+}
+
+const customname = document.getElementById('customname')
+const firstname = document.getElementById('fname')
+
+fname.addEventListener("input", function(type){
+    document.getElementById('customname').textContent =
+ "Hello " + type.target.value + ", I look forward to hearing from you!"
 })
